c02d48787d495cf4c87b5f9ee88c76ea5de547ad
HackerKhan7 <williammusyoka009@gmail.com>
2023-02-22 01:34:35 -0800

    print all natural numbers from n to 98

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 
+/**
+ * main - Print the first 50 Fibonacci numbers starting with 1 and 2,
+ * followed by a new line
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int count;
+	unsigned long fibi = 0, fibj = 1, sum;
 
-	/**
-	 * main - Print the first 50 Fibonacci numbers starting with 1 and 2,
-	 * followed by a new line
-	 * Return: Always 0.
-	 */
-	int main(void)
+	for (count = 0; count < 50; count++)
 	{
-		int count;
-		unsigned long fibi = 0, fibj = 1, sum;
+		sum = fibi + fibj;
+		printf("%lu", sum);
 
+		fibi = fibj;
+		fibj = sum;
 
-		for (count = 0; count < 50; count++)
-		{
-			sum = fibi + fibj;
-			printf("%lu", sum);
-
-
-			fibi = fibj;
-			fibj = sum;
-
-
-			if (count == 49)
-				printf("\n");
-			else
-				printf(", ");
-		}
-
-
-		return (0);
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
 	}
+
+	return (0);
+}
