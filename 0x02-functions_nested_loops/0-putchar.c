883840339d543ecbca18bb9564aa1967e93e7383
HackerKhan7 <williammusyoka009@gmail.com>
2023-02-22 00:21:22 -0800

    putchr

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * Return: Always 0 (Success)
-	 */
 
-
-	int main(void)
-	{
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -16,5 +15,7 @@
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
-	}
+}
+
